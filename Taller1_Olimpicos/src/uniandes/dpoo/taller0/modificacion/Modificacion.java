4759380ccb469d70c4a9c1c1ff14b2775158d1e4
acca4 <acca4@ZenBookAndres>
2023-08-22 19:30:02 -0600

    Commit

---
+++ Taller1_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,25 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola, mundo!");
+		CalculadoraEstadisticas calc;
+		try {
+			calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+			System.out.println(calc.paisConMasMedallistas());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
